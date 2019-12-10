57b21518763d9ad98618df09ae13c6220ec050e6
James Shen <jjss886@gmail.com>
2019-12-10 16:57:02 -0500

    MergeSort split function

--- mergesort.js
+++ mergesort.js
@@ -17,25 +17,21 @@ function merge(arr1, arr2) {
 }
 
 function mergeSort(arr) {
-
   const arrLen = arr.length;
   arr = split(arr);
   let i = 0;
 
-  console.log('entered mergeSort')
-  while( i < arrLen - 1 ) {
-    console.log('mergeSort while loop', i);
-    if (arr[i].length > 1 && !Array.isArray(arr[i])) {
+  while (i < arrLen - 1) {
+    if (arr[i].length === 1) {
       i++;
     } else {
-      arr.splice(i, 1, [...split(arr[i])]);
+      const result = split(arr[i]);
+      arr.splice(i, 1, result[0], result[1]);
     }
-    console.log('arr in while = ', arr)
-    if (i > 50) break;
   }
-  console.log('arr = ', arr);
-  const sortedArray = merge(splitArrays[0], splitArrays[1]);
-  console.log(sortedArray)
+  console.log("arr = ", arr);
+  // const sortedArray = merge(splitArrays[0], splitArrays[1]);
+  // console.log(sortedArray);
 }
 
-mergeSort([1,2,3,4,5,6,7])
+mergeSort([1, 2, 3, 4, 5, 6, 7]);
